c7a6ee285bc89fc177f42ce50b6096aae0092d4f
mahendra-16 <prajapatimahendra@ymail.com>
2016-11-01 20:24:18 +0530

    session2

---
+++ Assignment2.java
@@ -0,0 +1,14 @@
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,c;
+		b = 4;
+		
+		System.out.println("B : " + b);
+
+	}
+
+}
